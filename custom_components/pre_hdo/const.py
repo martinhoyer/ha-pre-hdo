e824a5d062c73bf78da9ee0aecc618b6f15b8122
Martin Hoyer <mhoyer@redhat.com>
2026-03-09 21:40:17 +0100

    Remove unused constants
    Remove DEFAULT_SCAN_INTERVAL (replaced by daily polling),
    CONF_PERIODS, CONF_PERIOD_NAME, CONF_PERIOD_MINUTES, and
    HDO_PAGE_URL which were all unused.

--- custom_components/pre_hdo/const.py
+++ custom_components/pre_hdo/const.py
@@ -7,16 +7,10 @@ DOMAIN = "pre_hdo"
 PRAGUE_TZ = ZoneInfo("Europe/Prague")
 
 CONF_RECEIVER_COMMAND_ID = "receiver_command_id"
-CONF_PERIODS = "periods"
-CONF_PERIOD_NAME = "name"
-CONF_PERIOD_MINUTES = "minutes"
 
 BASE_URL = "https://www.predistribuce.cz"
 HDO_ONE_DAY_URL = f"{BASE_URL}/com/PREdi/UI/Forms/Hdo/HdoForm:hdoOneDayAjax"
 HDO_MULTI_DAY_URL = f"{BASE_URL}/com/PREdi/UI/Forms/Hdo/HdoForm:hdoMoreDaysAjax"
-HDO_PAGE_URL = f"{BASE_URL}/cs/potrebuji-zaridit/zakaznici/stav-hdo/"
-
-DEFAULT_SCAN_INTERVAL = 900  # 15 minutes
 
 TARIFF_LOW = "NT"
 TARIFF_HIGH = "VT"
